ae31fb828959c0b9d63892810b0a62e5e0f34c23
Will Manson <willrtmanson@gmail.com>
2016-01-31 16:04:32 +0000

    Find starting position, add some error-handling and tidy up code

--- game.py
+++ game.py
@@ -1,11 +1,14 @@
+import sys
+
 class Game:
     def __init__(self, game_map):
         self.game_map = game_map
+        self.player_pos = self.find_start_position()
 
     def display_map(self):
-        for y, row in enumerate(self.game_map):
-            for x in row:
-                print(self.style_map_detail(x), end='')
+        for row in self.game_map:
+            for cell in row:
+                print(self.style_map_detail(cell), end='')
             print()
 
     def style_map_detail(self, detail):
@@ -15,3 +18,11 @@ class Game:
             '2': 'S',
             '3': 'E'
         }.get(detail, '?')
+
+    def find_start_position(self):
+        for y, row in enumerate(self.game_map):
+            for x, cell in enumerate(row):
+                if cell == '2':
+                    return (y, x)
+        print("No start position set")
+        sys.exit(1)
