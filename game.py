43086589d7a8ca41379319b04ebe21d7ecd01fad
Will Manson <willrtmanson@gmail.com>
2016-02-01 00:01:32 +0000

    Apply q-learning to monster avoidance

--- game.py
+++ game.py
@@ -1,15 +1,21 @@
 import sys
+import random
 
 class Game:
     def __init__(self, game_map):
         self.game_map = game_map
-        self.player_pos = self.find_start_position()
+        self.player_pos = self.find_position('2')
+        self.has_monster = self.check_has_monster()
+        if self.has_monster:
+            self.monster_pos = self.find_position('4')
 
     def display_map(self, show_player=True):
         for y, row in enumerate(self.game_map):
             for x, cell in enumerate(row):
                 if show_player and (y, x) == self.player_pos:
                     print('X', end='')
+                elif self.has_monster and show_player and (y, x) == self.monster_pos:
+                    print('M', end='')
                 else:
                     print(self.style_map_detail(cell), end='')
             print()
@@ -22,31 +28,67 @@ class Game:
             '0': '.',
             '1': 'O',
             '2': 'S',
-            '3': 'E'
+            '3': 'E',
+            '4': 'P'
         }.get(detail, '?')
 
-    def find_start_position(self):
+    def find_position(self, to_find):
         for y, row in enumerate(self.game_map):
             for x, cell in enumerate(row):
-                if cell == '2':
+                if cell == to_find:
                     return (y, x)
-        print('No start position set')
+        print('No position set')
         sys.exit(1)
 
+    def check_has_monster(self):
+        for row in self.game_map:
+            if '4' in row:
+                return True
+        return False
+
+    def get_relative_monster_location(self):
+        relative_x = self.player_pos[1] - self.monster_pos[1]
+        relative_y = self.player_pos[0] - self.monster_pos[0]
+        if (abs(relative_x) >= 3 or abs(relative_y) >= 3):
+            return 25
+        return 5 * (relative_x + 2) + relative_y + 2
+
     def is_dead(self):
-        return self.game_map[self.get_y()][self.get_x()] == '0'
+        return (self.game_map[self.get_y()][self.get_x()] == '0' or self.has_hit_monster())
 
     def has_won(self):
         return self.game_map[self.get_y()][self.get_x()] in [ '3' ]
 
     def respawn(self):
-        self.player_pos = self.find_start_position()
+        self.player_pos = self.find_position('2')
 
     def get_y(self):
         return self.player_pos[0]
 
     def get_x(self):
         return self.player_pos[1]
+
+    def update_monster(self):
+        if self.has_monster:
+            random_num = random.random()
+            if random_num < 0.25:
+                self.monster_pos = (self.monster_pos[0] + 1, self.monster_pos[1])
+            elif random_num < 0.50:
+                self.monster_pos = (self.monster_pos[0] - 1, self.monster_pos[1])
+            elif random_num < 0.75:
+                self.monster_pos = (self.monster_pos[0], self.monster_pos[1] + 1)
+            else:
+                self.monster_pos = (self.monster_pos[0], self.monster_pos[1] - 1)
+            if self.game_map[self.monster_pos[0]][self.monster_pos[1]] == '0':
+                self.respawn_monster()
+
+    def respawn_monster(self):
+        self.monster_pos = self.find_position('4')
+
+    def has_hit_monster(self):
+        if self.has_monster:
+            return (self.player_pos[0] == self.monster_pos[0] and self.player_pos[1] == self.monster_pos[1])
+        return False
 
     def perform_action(self, action):
         if action == 'w':
@@ -59,3 +101,4 @@ class Game:
             self.player_pos = (self.get_y(), self.get_x() + 1)
         else:
             print('Invalid action')
+        self.update_monster()
---
+++ monster-q-learner.py
@@ -0,0 +1,104 @@
+import random
+import math
+import time
+from game import Game
+from file_manager import FileManager
+
+class QLearner:
+    def __init__(self, game, available_actions, discount_factor=0.99, learning_rate=0.9):
+        self.game = game
+        self.available_actions = available_actions
+        dimensions = self.game.get_map_dimensions()
+        self.q = [[[[0.0 for action in self.available_actions] for x in range(dimensions[1])] for y in range(dimensions[0])] for monster in range(26)]
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+
+    def perform_action(self, action):
+        self.game.perform_action(action)
+        if self.game.is_dead():
+            return 'dead'
+        elif self.game.has_won():
+            return 'won'
+        else:
+            return 'nothing'
+
+    def iterate(self):
+        x = self.game.get_x()
+        y = self.game.get_y()
+        monster = self.game.get_relative_monster_location()
+        action_index = self.choose_action()
+        response = self.perform_action(self.available_actions[action_index])
+
+        new_x = self.game.get_x()
+        new_y = self.game.get_y()
+        new_monster = self.game.get_relative_monster_location()
+
+        reward = 0.0
+        if response == 'dead':
+            reward = -25.0
+        elif response == 'won':
+            reward = 100.0
+
+        self.q[monster][y][x][action_index] = self.q[monster][y][x][action_index] + self.learning_rate * (reward + self.discount_factor * max(self.q[new_monster][new_y][new_x]) - self.q[monster][y][x][action_index])
+        if response == 'dead' or response == 'won':
+            print(response)
+            self.game.respawn()
+
+    def choose_action(self):
+        q_list_at_pos = self.get_q_list_at_pos()
+        q_list_at_pos = [math.exp(x) for x in q_list_at_pos]
+
+        cumulative_sum = self.get_cumulative_sum(q_list_at_pos)
+
+        random_num = random.random() * cumulative_sum[-1]
+        action_index = 0
+        while True:
+            if action_index == len(q_list_at_pos) - 1:
+                break
+            elif random_num > cumulative_sum[action_index]:
+                action_index += 1
+            else:
+                break
+
+        return action_index
+
+    def get_q_list_at_pos(self):
+        return self.q[game.get_relative_monster_location()][game.get_y()][game.get_x()]
+
+    def get_cumulative_sum(self, list_to_sum):
+        cumulative_list = [ ]
+        for i, x in enumerate(list_to_sum):
+            cumulative_list.append(x if i == 0 else cumulative_list[i - 1] + x)
+        return cumulative_list
+
+random.seed()
+available_actions = [ 'w', 'a', 's', 'd' ]
+game = Game(FileManager().load_map('arena'))
+learner = QLearner(game, available_actions)
+
+while True:
+    print()
+    print('Options:')
+    print('m: Output map')
+    print('t: Train')
+    print('r: Respawn')
+    print('o: Output quantity data')
+    print('q: Quit')
+    choice = input('Choose: ')
+
+    if choice == 'm':
+        learner.game.display_map()
+    elif choice == 't':
+        num = input('Number of iterations: ')
+        to_watch = input('Watch? y/n: ') == 'y'
+        for i in range(int(num)):
+            learner.iterate()
+            if to_watch:
+                learner.game.display_map()
+                time.sleep(0.3)
+    elif choice == 'r':
+        learner.game.respawn()
+    elif choice == 'o':
+        print(learner.q)
+    elif choice == 'q':
+        break
